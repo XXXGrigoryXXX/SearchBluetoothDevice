726e034b824f209ef894a6dc207357ae91ee8900
XXXGrigoryXXX <grigorij.ivanov.1997@list.ru>
2021-09-06 00:24:04 +0300

    search device

---
+++ app/src/main/java/com/example/searchbluetoothdevice/MainActivity.java
@@ -0,0 +1,259 @@
+package com.example.searchbluetoothdevice;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements
+        CompoundButton.OnCheckedChangeListener,
+        AdapterView.OnItemClickListener,
+        View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_LOC = 1;
+
+    private static final int REQ_ENABLE_BT = 10;
+    private static final int BT_BOUNDED    = 21;
+    private static final int BT_SEARCH     = 22;
+
+    private FrameLayout frameMessage;
+    private LinearLayout frameControl;
+
+    private Switch switchEnableBt;
+    private Button btnEnableSearch;
+    private ProgressBar pbProgress;
+    private ListView listBtDevices;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BtListAdapter listAdapter;
+    private ArrayList<BluetoothDevice> bluetoothDevices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameMessage = findViewById(R.id.frame_message);
+        frameControl = findViewById(R.id.frame_control);
+
+        switchEnableBt  = findViewById(R.id.switch_enable_bt);
+        btnEnableSearch = findViewById(R.id.btn_enable_search);
+        pbProgress      = findViewById(R.id.pb_progress);
+        listBtDevices   = findViewById(R.id.lv_btn_device);
+
+        switchEnableBt.setOnCheckedChangeListener(this);
+        btnEnableSearch.setOnClickListener(this);
+        listBtDevices.setOnItemClickListener(this);
+
+        bluetoothDevices = new ArrayList<>();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, R.string.bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onCreate: " + getString(R.string.bluetooth_not_supported));
+            finish();
+        }
+
+        if (bluetoothAdapter.isEnabled()) {
+            showFrameControls();
+            switchEnableBt.setChecked(true);
+            setListAdapter(BT_BOUNDED);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View v) {
+        if (v.equals(btnEnableSearch)) {
+            enableSearch();
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (buttonView.equals(switchEnableBt)) {
+            enableBt(isChecked);
+
+            if (!isChecked) {
+                showFrameMessage();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQ_ENABLE_BT) {
+            if (resultCode == RESULT_OK && bluetoothAdapter.isEnabled()) {
+                showFrameControls();
+                setListAdapter(BT_BOUNDED);
+            } else if (resultCode == RESULT_CANCELED) {
+                enableBt(true);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void showFrameMessage() {
+        frameMessage.setVisibility(View.VISIBLE);
+        frameControl.setVisibility(View.GONE);
+    }
+
+    private void showFrameControls() {
+        frameMessage.setVisibility(View.GONE);
+        frameControl.setVisibility(View.VISIBLE);
+    }
+
+    private void enableBt(boolean flag) {
+        if (flag) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, REQ_ENABLE_BT);
+        } else {
+            bluetoothAdapter.disable();
+        }
+    }
+
+    private void setListAdapter(int type) {
+
+        bluetoothDevices.clear();
+        int iconType = R.drawable.ic_bluetooth_bounded_device;
+
+        switch (type) {
+            case BT_BOUNDED:
+                bluetoothDevices = getBoundedDevice();
+                iconType = R.drawable.ic_bluetooth_bounded_device;
+                break;
+            case BT_SEARCH:
+                iconType = R.drawable.ic_bluetooth_search_device;
+                break;
+        }
+        listAdapter = new BtListAdapter(this, bluetoothDevices, iconType);
+        listBtDevices.setAdapter(listAdapter);
+    }
+
+    private ArrayList<BluetoothDevice> getBoundedDevice() {
+        Set<BluetoothDevice> deviceSet = bluetoothAdapter.getBondedDevices();
+        ArrayList<BluetoothDevice> tmpArrayList = new ArrayList<>();
+        if (deviceSet.size() > 0) {
+            for (BluetoothDevice device: deviceSet) {
+                tmpArrayList.add(device);
+            }
+        }
+
+        return tmpArrayList;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void enableSearch() {
+        if (bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+        } else {
+            accessLocationPermission();
+            bluetoothAdapter.startDiscovery();
+        }
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            switch (action) {
+                case BluetoothAdapter.ACTION_DISCOVERY_STARTED:
+                    btnEnableSearch.setText(R.string.stop_search);
+                    pbProgress.setVisibility(View.VISIBLE);
+                    break;
+                case BluetoothAdapter.ACTION_DISCOVERY_FINISHED:
+                    btnEnableSearch.setText(R.string.start_search);
+                    pbProgress.setVisibility(View.GONE);
+                    break;
+                case BluetoothDevice.ACTION_FOUND:
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    if (device != null) {
+                        bluetoothDevices.add(device);
+                        listAdapter.notifyDataSetChanged();
+                    }
+                    break;
+            }
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void accessLocationPermission() {
+        int accessCoarseLocation = this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+        int accessFineLocation = this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        List<String> listRequestPermission = new ArrayList<String>();
+
+        if (accessCoarseLocation != PackageManager.PERMISSION_GRANTED) {
+            listRequestPermission.add(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if (accessFineLocation != PackageManager.PERMISSION_GRANTED) {
+            listRequestPermission.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (!listRequestPermission.isEmpty()) {
+            String[] strRequestPermission = listRequestPermission.toArray(new String[listRequestPermission.size()]);
+            this.requestPermissions(strRequestPermission, REQUEST_CODE_LOC);
+        }
+    }
+
+    private void onRequestPermissionResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_LOC:
+
+                if (grantResults.length > 0) {
+                    for (int gr : grantResults) {
+                        if (gr != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+}
